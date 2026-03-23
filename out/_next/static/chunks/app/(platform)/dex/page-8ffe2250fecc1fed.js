217383e6d007f4fc327445370d9113483c0d3dcc
atarawendesidkabore-hash <atarawendesidkabore-hash@users.noreply.github.com>
2026-03-23 16:55:14 +0100

    Deploy WASI Platform static site

---
+++ out/_next/static/chunks/app/(platform)/dex/page-8ffe2250fecc1fed.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[437,562],{7228:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,1621))}},function(e){e.O(0,[851,972,621,971,117,744],function(){return e(e.s=7228)}),_N_E=e.O()}]);
